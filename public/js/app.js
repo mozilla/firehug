4a995fdecac59f43c3d0fab2051785a8d5ccf22e
Harald Kirschner <hkirschner@mozilla.com>
2013-10-02 16:16:02 -0700

    Fixed GA, wasn't executed due to CSP Adding spinner for schedule, fixes #91

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,18 @@
 (function() {
   'use strict';
 
+  window._gaq = [];
+  _gaq.push(['_setAccount', 'UA-35433268-40']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
+  })();
+
   var origin = location.protocol + '//' + location.host;
 
   // FastClick.attach(document.body);
@@ -204,7 +216,7 @@
       });
 
       $scope.$on('$viewContentLoaded', function(event) {
-        $window._gaq.push(['_trackPageview', $location.path()]);
+        _gaq.push(['_trackPageview', $location.path()]);
       });
 
       // Watch login and redirect as needed
@@ -405,6 +417,7 @@
         url: '/schedule',
         method: 'GET'
       }).then(function(data) {
+        $scope.loaded = true;
         for (var s in data.data.schedule) {
           var evt = data.data.schedule[s];
 
